a9844f3ec1d3c4760ce20ef04d0b7737b18cbab4
Aarya Jha <aj240502@gmail.com>
2022-04-27 01:39:15 +0530

    basic setup done, adding and deleting functional

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer align="center">
+        2022 © Chirag Kapil
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/ToDoList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Todo from "./Todo";
+
+const ToDoList = ({todos,setTodos}) => {
+    return (
+        <div className="todo-container">
+            <ul className = 'todo-list'>
+                {todos.map((todo)=>(
+                    <Todo setTodos ={setTodos} key = {todo.id} todos = {todos} todo={todo} text = {todo.text} />
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default ToDoList;
